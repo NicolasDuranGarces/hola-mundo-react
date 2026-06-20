297355c49b429d3ba177037d161b4350cf998f45
Nicolas Duran Garces <niduga@outlook.es>
2026-06-20 00:25:22 -0500

    Hola Mundo React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hola Mundo</h1>
+      <p>Mi primera app en React desplegada en Vercel</p>
+    </div>
+  )
+}
+
+export default App
